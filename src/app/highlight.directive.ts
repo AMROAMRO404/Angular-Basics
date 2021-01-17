b90bdb6aa1a88b9f44714456b4b06d06430ad8ae
AMROAMRO404 <amro.amro1999@gmail.com>
2021-01-17 13:00:55 +0200

    hostBinding

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,22 +1,34 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective {
-  constructor(private el: ElementRef) {
+  constructor(private el: ElementRef, private render: Renderer2) {
     el.nativeElement.style.backgroundColor = 'blue';
   }
   @Input('appHighlight') highlightColor: any;
-
+  @Input() bStyle: string = 'solid';
   @HostListener('mouseenter') onMouseLeave() {
     this.highlight('red');
   }
   @HostListener('mouseleave') onMouseEnter() {
     this.highlight('blue');
   }
+  @HostBinding('style.borderStyle')
+  get style() {
+    return this.bStyle;
+  }
 
   private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color;
+    // this.el.nativeElement.style.backgroundColor = color;
+    this.render.setStyle(this.el.nativeElement, 'backgroundColor', color);
   }
 }
